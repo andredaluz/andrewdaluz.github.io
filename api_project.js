1176ddbb32681c493c109ac8c8a72332d823d133
André Felipe <andre@trezo.com.br>
2016-11-17 18:08:27 -0200

    Api doc

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "example",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "title": "Custom apiDoc browser title",
+  "url": "https://api.github.com/v1",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-11-17T20:08:06.710Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
